e29c24db93a1f135ffb45b68e5f3aae159b81fa4
WeichenXu <weichen.xu@databricks.com>
2020-01-14 01:49:49 +0800

    Support for dynamic allocation (#10)
    Support for dynamic allocation, always set the thread pool size to n_jobs but throws a warning if current spark.maxNumConcurrenttaks is smaller than n_jobs.
    
    Closes #7.

--- test/test_backend.py
+++ test/test_backend.py
@@ -16,5 +16,5 @@ def test_effective_n_jobs():
 
     with warnings.catch_warnings(record=True) as w:
         warnings.simplefilter("always")
-        assert backend.effective_n_jobs(n_jobs=16) == 8
+        assert backend.effective_n_jobs(n_jobs=16) == 16
         assert len(w) == 1
