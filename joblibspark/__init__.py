b80640980fe1a292cccb8b54ddf30df2305f1ea7
Weichen Xu <weichen.xu@databricks.com>
2020-05-11 16:56:08 +0800

    release 0.2.0

--- joblibspark/__init__.py
+++ joblibspark/__init__.py
@@ -18,7 +18,7 @@
 """
 Joblib spark backend is a extension for joblib, which make joblib running on spark parallelly.
 """
-__version__ = '0.1.0.dev'
+__version__ = '0.2.0'
 
 
 def register_spark():
