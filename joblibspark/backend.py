a9d09a4acbf25c24f817a8aaf7782196b53b8f4b
Lu Wang <38018689+lu-wang-dl@users.noreply.github.com>
2020-11-17 18:31:15 -0800

    Update joblib-spark with pyspark 3.1 (#28)
    Update joblib-spark with pyspark 3.1

--- joblibspark/backend.py
+++ joblibspark/backend.py
@@ -96,7 +96,12 @@ class SparkDistributedBackend(ParallelBackendBase, AutoBatchingMixin):
     def _get_max_num_concurrent_tasks(self):
         # maxNumConcurrentTasks() is a package private API
         # pylint: disable=W0212
-        return self._spark.sparkContext._jsc.sc().maxNumConcurrentTasks()
+        pyspark_version = VersionUtils.majorMinorVersion(pyspark.__version__)
+        spark_context = self._spark.sparkContext._jsc.sc()
+        if pyspark_version < (3, 1):
+            return spark_context.maxNumConcurrentTasks()
+        return spark_context.maxNumConcurrentTasks(
+                 spark_context.resourceProfileManager().resourceProfileFromId(0))
 
     def abort_everything(self, ensure_ready=True):
         self._cancel_all_jobs()
